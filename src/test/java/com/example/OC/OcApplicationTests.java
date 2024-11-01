d1bde2ffe28f08d6cf0829dfa281b76b87c6a3a2
Kim MinSeo <86477146+minseid@users.noreply.github.com>
2024-11-01 21:15:13 +0900

    start

---
+++ src/test/java/com/example/OC/OcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
