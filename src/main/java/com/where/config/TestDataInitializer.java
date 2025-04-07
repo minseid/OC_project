77185e10e4f70cfca4a3d34e8c8a8644223de8c7
Kim MinSeo <86477146+minseid@users.noreply.github.com>
2025-04-07 21:11:18 +0900

    S3 유저관련 수정

--- src/main/java/com/where/config/TestDataInitializer.java
+++ src/main/java/com/where/config/TestDataInitializer.java
@@ -1,6 +1,11 @@
 package com.where.config;
 
+import com.where.constant.UserRole;
+import com.where.entity.User;
+import com.where.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -8,26 +13,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class TestDataInitializer {
     private final PasswordEncoder passwordEncoder;
-//
-//    @Bean
-//    public CommandLineRunner initDatabase(UserRepository userRepository) {
-//        return args -> {
-//            User testUser = User.builder()
-//                    .email("test@example.com")
-//                    .password(passwordEncoder.encode("Test@1234"))
-//                    .nickName("테스트유저")
-//                    .profileImage("https://example.com/default-profile.png")
-//                    .role(UserRole.USER)
-//                    .build();
-//
-//            if (userRepository.findByEmail(testUser.getEmail()).isEmpty()) {
-//                userRepository.save(testUser);
-//                System.out.println("테스트 유저가 DB에 삽입되었습니다: " + testUser);
-//            } else {
-//                System.out.println("테스트 유저가 이미 존재합니다.");
-//            }
-//        };
-//    }
+
+    @Bean
+    public CommandLineRunner initDatabase(UserRepository userRepository) {
+        return args -> {
+            User testUser = User.builder()
+                    .email("test@example.com")
+                    .password(passwordEncoder.encode("Test@1234"))
+                    .nickName("테스트유저")
+                    .role(UserRole.USER)
+                    .build();
+
+            if (userRepository.findByEmail(testUser.getEmail()).isEmpty()) {
+                userRepository.save(testUser);
+                System.out.println("테스트 유저가 DB에 삽입되었습니다: " + testUser);
+            } else {
+                System.out.println("테스트 유저가 이미 존재합니다.");
+            }
+        };
+    }
 //
 //    @Bean
 //    public CommandLineRunner initMeetingDatabase(MeetingRepository meetingRepository, UserRepository userRepository, UserMeetingMappingRepository userMeetingMappingRepository) {
