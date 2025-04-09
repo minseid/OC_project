b5e02371ce69c0a0dc5d3fee59c5fe785588fc3c
MongNam <ppk052@naver.com>
2025-04-09 23:40:00 +0900

    4/9 초대장정보조회 오류수정

--- src/main/java/com/where/service/MeetingService.java
+++ src/main/java/com/where/service/MeetingService.java
@@ -52,7 +52,7 @@ public class MeetingService {
     private final ScheduleRepository scheduleRepository;
     private final FriendRepository friendRepository;
 
-    private final String linkForInvite = "https://www.audiwhere.codns.com/invite/";
+    private final String linkForInvite = "https://audiwhere.codns.com/invite/";
     private final LinkRepository linkRepository;
     private final UserPlaceMappingRepository userPlaceMappingRepository;
 
@@ -634,9 +634,13 @@ public class MeetingService {
 
     //초대장조회 메서드
     public GetInviteResponse getInvite(String link) {
-        if(meetingRepository.existsByLink("https://audiwhere.codns.com/invite/" + link)) {
-            Meeting targetMeeting = meetingRepository.findByLink(link).get();
+        String targetLink = "https://audiwhere.codns.com/invite/"+link;
+        if(meetingRepository.existsByLink(targetLink)) {
+            log.info("초대장있음");
+            Meeting targetMeeting = meetingRepository.findByLink(targetLink).get();
+            log.warn(targetMeeting.toString());
             if(scheduleRepository.existsByMeeting(targetMeeting)) {
+                log.warn("스케쥴있음");
                 Schedule schedule = scheduleRepository.findByMeeting(targetMeeting).get();
                 return GetInviteResponse.builder()
                         .meetingId(targetMeeting.getId())
@@ -646,6 +650,7 @@ public class MeetingService {
                         .scheduleTime(schedule.getTime())
                         .build();
             } else {
+                log.warn("스케쥴없음");
                 return GetInviteResponse.builder()
                         .meetingId(targetMeeting.getId())
                         .image(targetMeeting.getImage())
