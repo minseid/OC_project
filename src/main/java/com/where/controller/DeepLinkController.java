10d4958d092bef6edfd5525ac12dd684af17a785
정승민 <ppk052@naver.com>
2025-04-21 15:53:48 +0900

    4/21 초대장에 쿼리스트링으로 오는것 그대로 전달하는것 추가

--- src/main/java/com/where/controller/DeepLinkController.java
+++ src/main/java/com/where/controller/DeepLinkController.java
@@ -1,16 +1,20 @@
 package com.where.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Controller
@@ -33,9 +37,10 @@ public class DeepLinkController {
     }
 
     @GetMapping("/invite/{id}")
-    public String deepLinkWebpage(@PathVariable("id") String inviteLink, Model model) {
+    public String deepLinkWebpage(@PathVariable("id") String inviteLink, @RequestParam String name, Model model) {
         String uuid = UUID.randomUUID().toString();
-        model.addAttribute("inviteLink", "audiwhere://invite/" + inviteLink);
+        String nameForLink = URLEncoder.encode(name, StandardCharsets.UTF_8);
+        model.addAttribute("inviteLink", "audiwhere://invite/" + inviteLink +"?name=" + nameForLink );
         model.addAttribute("uuid", uuid);
         return "invite copy";
     }
