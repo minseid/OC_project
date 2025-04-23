37e177f7a86947d508011588dd889c19f0095742
Kim MinSeo <86477146+minseid@users.noreply.github.com>
2025-04-24 07:39:29 +0900

    mail service 구현

---
+++ src/main/java/com/where/security/mail/MailService.java
@@ -0,0 +1,64 @@
+package com.where.security.mail;
+
+import com.google.api.client.util.Value;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("{spring.mail.username}")
+    private static String senderEmail;
+
+    public String createCode() {
+        Random random = new Random();
+        StringBuilder key = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) { // 인증 코드 6자리
+            int index = random.nextInt(2); // 0~1까지 랜덤, 랜덤값으로 switch문 실행
+
+            switch (index) {
+                case 0 -> key.append((char) (random.nextInt(26) + 65)); // 대문자
+                case 1 -> key.append(random.nextInt(10)); // 숫자
+            }
+        }
+        return key.toString();
+    }
+
+    public MimeMessage createMail(String mail, String authCode) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.setFrom(senderEmail);
+        message.setRecipients(MimeMessage.RecipientType.TO, mail);
+        message.setSubject("이메일 인증");
+        String body = "";
+        body += "<h3>요청하신 인증 번호입니다.</h3>";
+        body += "<h1>" + authCode + "</h1>";
+        body += "<h3>감사합니다.</h3>";
+        message.setText(body, "UTF-8", "html");
+
+        return message;
+    }
+
+    // 메일 발송
+    public boolean sendSimpleMessage(String sendEmail) throws MessagingException {
+        String authCode = createCode(); // 랜덤 인증번호 생성
+
+        MimeMessage message = createMail(sendEmail, authCode); // 메일 생성
+        try {
+            javaMailSender.send(message); // 메일 발송
+            return true;
+        } catch (MailException e) {
+            return false;
+        }
+    }
+}
